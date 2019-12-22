adcfb41958994461593019e05f70cafcfaf06b68
Kenny <kreyes@plainconcepts.com>
2019-12-22 18:06:18 +0100

    Add a first aproximation of scaffolding

---
+++ frontend/src/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace frontend.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required] [StringLength(200)] public string Name { get; set; }
+        [Required] [StringLength(16)] public string Vat { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
---
+++ frontend/src/MyAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using frontend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace frontend
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext(DbContextOptions<MyAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasData(new Customer() {Id = 1, Name = "Microsoft", Vat = "IE8256796U", Enabled = true},
+                    new Customer() {Id = 2, Name = "Google", Vat = "IE6388047V", Enabled = false});
+        }
+    }
+}
