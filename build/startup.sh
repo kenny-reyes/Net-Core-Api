fc6a39a8e0b96fcd3495f2d07505806ebe5ae63b
Kenny <kreyes@plainconcepts.com>
2019-12-26 04:36:07 +0100

    Add compose and some improvements

---
+++ build/startup.sh
@@ -0,0 +1,2 @@
+dotnet ef database update 
+dotnet watch run --urls "http://0.0.0.0:5000;https://0.0.0.0:5001"
---
+++ src/startup.sh
@@ -0,0 +1,3 @@
+pwd
+dotnet ef database update 
+dotnet watch run --urls "http://0.0.0.0:5000;https://0.0.0.0:5001"
